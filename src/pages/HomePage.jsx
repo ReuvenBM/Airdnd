5bf4ad5a7348ab44ae66d565dee6d0fe946dc194
Daria Rosen <daria.sk135@gmail.com>
2025-07-23 21:36:55 +0300

    Add condition to continue-searching functionality

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,9 +11,8 @@ const dateRange = getFormattedDateRange() // e.g. "Jul 15–17"
 
 export function HomePage() {
   const filterBy = useSelector((storeState) => storeState.homeModule.filterBy)
-
+  const loggedInUser = useSelector(storeState => storeState.userModule.loggedInUser)
   const setExistSearchParams = useFilterSearchParams()
-
   const arrow1 = "/Airdnd/icons/arrow1.svg"
 
   useEffect(() => {
@@ -26,20 +25,22 @@ export function HomePage() {
 
   return (
     <div className="main-page">
-      <hr />
-
-      <div className="continue-searching">
-        Continue searching for homes in Athens {dateRange} – 1 guest{" "}
-        <div className="arrow-icon-wrapper">
-          <img
-            src={arrow1}
-            alt="Arrow icon"
-            className="arrow-icon-gray-circle"
-          />
+      {loggedInUser?.searching_history?.length > 0 && (
+        <div className="continue-searching">
+          <hr />
+          Continue searching for homes in Athens {dateRange} – 1 guest{" "}
+          <div className="arrow-icon-wrapper">
+            <img
+              src={arrow1}
+              alt="Arrow icon"
+              className="arrow-icon-gray-circle"
+            />
+          </div>
+          <hr />
         </div>
-      </div>
+      )}
 
-      <hr />
+
 
       <div className="section-wrapper">
         <div className="text-with-icon">
